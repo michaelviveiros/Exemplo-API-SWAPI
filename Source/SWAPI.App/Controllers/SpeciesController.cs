04a96218698c5257301a01b3e30e47856eac2665
Michael Viveiros <maykonviveiros@gmail.com>
2024-01-08 16:32:10 -0300

    feat: Implementado a estrutura de consulta da entidade [Species] conforme tarefa abaixo. Tarefa associada: #7

---
+++ Source/SWAPI.App/Controllers/SpeciesController.cs
@@ -0,0 +1,36 @@
+﻿using SWAPI.App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SWAPI.App.Controllers
+{
+    public class SpeciesController : SWAPIController
+    {
+        [HttpGet]
+        public async Task<ActionResult> Index(int? id)
+        {
+            if (ModelState.IsValid)
+            {
+                SwapiSpeciesViewModel model = null;
+
+                try
+                {
+                    model = await ObterDados_Especies(id, Enums.TypeRequest.Full);
+
+                    return View(model);
+                }
+
+                catch (Exception ex)
+                {
+
+                }
+            }
+
+            return View();
+        }
+    }
+}
